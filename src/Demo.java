ba3b5fbe7d3e8647e2929d3c30da3ac869b12c0a
moocstudent <343840681@qq.com>
2021-03-29 08:48:23 +0800

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,6 @@
+/**
+ * @Author: zhangQi
+ * @Date: 2021-03-27 17:24
+ */
+public class Demo {
+}
---
+++ src/Simple.java
@@ -0,0 +1,18 @@
+/**
+ * @Author: zhangQi
+ * @Date: 2021-03-27 16:19
+ */
+public class Simple {
+
+    public static void main(String[] args) {
+//        final String helloWorld = "Hello World";
+//        System.out.println(helloWorld);
+        math();
+    }
+
+    public static void math(){
+        final int x = 10 * 10;
+        System.out.println(x);
+    }
+
+}
